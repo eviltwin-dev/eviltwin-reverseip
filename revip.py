e08c29fb9a322992e15ddd5d0168cfd6c9c41b5c
Evil Twin <63768005+eviltwin-dev@users.noreply.github.com>
2020-04-26 13:18:24 +0700

    Add files via upload

---
+++ revip.py
@@ -0,0 +1,48 @@
+import time
+import requests
+import os,sys
+from termcolor import colored
+os.system("clear")
+print(colored(""" 
+      ____
+    ,'    '.
+   /  RevIP \\
+   \ ()  () /
+    `. /\ ,'
+×EVIL| "" |TWIN×
+     'UUUU'
+"""
+,"""red"""))
+def rev():
+	try:
+		inp=input(colored("Target IP: ", "white"))
+		if inp=='':
+			print(colored("No Address Found !", "red"))
+			sys.exit()
+		if inp.startswith('http://'):
+			print(colored("Cant Use 'http://' ", "red"))
+			sys.exit()
+		if inp.startswith("https://"):
+			sys.exit(colored("Cant Use 'https://' ", "red"))
+
+		get=requests.get(f"http://api.hackertarget.com/reverseiplookup/?q={inp}")
+		file=open(f"./{inp}.txt", "w")
+		if get.text== "error check your search parameter":
+			sys.exit(colored(f"(!) '{inp}' Not Valid Address Please Check Your Brain !", "red"))
+		elif get.text==f"No DNS A records Found For {inp}":
+			print(colored(f"\n(!) No DNS Found On {inp}\n","red"))
+			sys.exit()
+		else:
+			print(file.write(str(get.text)))
+			print(colored(f"(✓) Successfully Check {inp}.txt file.", "red"))
+	except Exception:
+		print(colored(f"Are You Poor ? Please Buy Connection Internet", "red"))
+	except KeyboardInterrupt:
+		x=input(colored("\n(!) Sure Close ? {Y/N}: ","red"))
+		if x=="Y" or x=="y":
+			sys.exit
+		elif x=="N" or x=="n":
+			rev()
+		else:
+			sys.exit(colored("(!) Application disconnected.","red"))
+rev()
